7414ccc2112d48dcd12a487d6729cf67950e499e
Dmitriy Fokin <fokinds1@gmail.com>
2020-05-30 01:45:47 +0600

    first commit

---
+++ src/main/java/ru/fds/tavrzcms_export/TavrzcmsExportApplication.java
@@ -0,0 +1,13 @@
+package ru.fds.tavrzcms_export;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavrzcmsExportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TavrzcmsExportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fds/tavrzcms_export/TavrzcmsExportApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fds.tavrzcms_export;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TavrzcmsExportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
